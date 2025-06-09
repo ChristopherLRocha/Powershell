6ce0de85906bceca4a4d36453330b3f72a9c9678
NeuroPunk808 <neuropunk808@gmail.com>
2025-06-09 08:31:37 -0500

    Updated List to use words from a document

--- AD/Password_Generator.ps1
+++ AD/Password_Generator.ps1
@@ -1,11 +1,21 @@
 ## Simple password generator. Combines 3 words from an array, the last digit of the current year, and '!'
+## Uses the list from https://gist.github.com/shmookey/b28e342e1b1756c4700f42f17102c2ff
 
-$word_list = @('Drown','Class','Ghost','Evoke','Exile','Split','Brush','Alarm','Clear','Tower','Handy','Guide','Strap','Grant','Server','Fight','Amber','Table')
+# Path to your word list file
+$word_list_path = "\\Path\to\Words.txt"
 
-$password = ($word_list | Get-Random -Count 3) -join ''
+# Import the word list from the text file
+$word_list = Get-Content $word_list_path
 
+# Select 3 random words and capitalize the first letter of each
+$password = ($word_list | Get-Random -Count 3 | ForEach-Object {
+    $_.Substring(0,1).ToUpper() + $_.Substring(1).ToLower()
+}) -join ''
+
+# Add the last digit of the current year and a symbol
 $last_digit_year = (Get-Date).Year.ToString()[-1]
 
 $password += $last_digit_year += '!'
 
+# Output the password
 write-output $password
