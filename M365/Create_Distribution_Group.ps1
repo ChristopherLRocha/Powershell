3cf3947b210e7ae0b8426f890f9f56c074d29fd1
NeuroPunk808 <neuropunk808@gmail.com>
2025-06-11 12:17:16 -0500

    removed

--- M365/Create_Distribution_Group.ps1
+++
@@ -1,6 +0,0 @@
-Set-ExecutionPolicy Bypass
-$Distribution_Group = Read-Host -Prompt("What would you like the Distribution Group to be called?")
-
-## Connect to Exchange Online
-Connect-ExchangeOnline
-
