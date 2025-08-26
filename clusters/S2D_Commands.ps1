8d497e6a0a29c94303e95acc526629509b5517ef
NeuroPunk808 <neuropunk808@gmail.com>
2025-08-26 12:36:20 -0500

    Added some maintenance S2D_Commands

--- clusters/S2D_Commands.ps1
+++ clusters/S2D_Commands.ps1
@@ -12,3 +12,20 @@ Write-output "Setting the CSV cache..."
 Get-PhysicalDisk | Select-Object -Property Friendlyname, ClassName, CanPool, CannotPoolReason | Format-Table -AutoSize
 
 Get-PhysicalDisk | Format-List
+
+#Get health
+Get-ClusterS2D
+
+Get-StoragePool
+
+Get-StorageSubSystem -FriendlyName *Cluster* | Get-StorageHealthReport
+
+#Verify that all the physical and virtual drives are healthy by running the following commands:
+Get-physicaldisk
+Get-virtualdisks
+
+#Check if there are backend repair jobs
+Get-StorageJob
+
+#Put storage in Maintenance mode
+Get-StorageFaultDomain -type StorageScaleUnit | Where-Object {$_.Friendlyname -eq "<Servername>"} | enable-StorageMaintenanceMode
